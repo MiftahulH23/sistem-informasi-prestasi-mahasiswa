8021e183b80a5127697568b05032de8beadf8793
MiftahulH23 <miftahul21si@mahasiswa.pcr.ac.id>
2025-02-02 15:24:33 +0700

    first commit

---
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
+class SocialiteController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callback()
+    {
+        $socialUser = Socialite::driver('google')->user();
+
+        $registeredUser = User::where('google_id', $socialUser->id)->first();
+
+        if ($registeredUser){
+            $registeredUser->update([
+                'name' => $socialUser->name,
+                'email' => $socialUser->email,
+                'password' => Hash::make('123'),
+                'google_id' => $socialUser->id,
+                'google_token' => $socialUser->token,
+                'google_refresh_token' => $socialUser->refreshToken,
+            ]);
+            Auth::login($registeredUser);
+            return redirect('/dashboard');
+        } else {
+            $user = User::create([
+                'name' => $socialUser->name,
+                'email' => $socialUser->email,
+                'password' => Hash::make('123'),
+                'google_id' => $socialUser->id,
+                'google_token' => $socialUser->token,
+                'google_refresh_token' => $socialUser->refreshToken,
+            ]);
+
+            Auth::login($user);
+            return redirect('/dashboard');
+        }
+
+        // $user->token;
+    }
+
+    // public function isStudent($email)
+    // {
+    //     $response = Http::get('https://api.example.com/student', [
+    //         'email' => $email,
+    //     ]);
+    // }
+}
---
+++ resources/js/Components/login-form.jsx
@@ -0,0 +1,99 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Head, Link, useForm } from '@inertiajs/react';
+
+export function LoginForm({ className, ...props }) {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        email: "",
+        password: "",
+        remember: false,
+    });
+    const submit = (e) => {
+        e.preventDefault();
+
+        post(route("login"), {
+            onFinish: () => reset("password"),
+        });
+    };
+    return (
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form onSubmit={submit}>
+                        <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    value={data.email}
+                                    className="mt-1 block w-full"
+                                    autoComplete="username"
+                                    isFocused={true}
+                                    onChange={(e) =>
+                                        setData("email", e.target.value)
+                                    }
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Password</Label>
+                                    <a
+                                        href="#"
+                                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                    >
+                                        Forgot your password?
+                                    </a>
+                                </div>
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    value={data.password}
+                                    className="mt-1 block w-full"
+                                    autoComplete="current-password"
+                                    onChange={(e) =>
+                                        setData("password", e.target.value)
+                                    }
+                                    required
+                                />
+                            </div>
+                            <Button type="submit" className="w-full">
+                                Login
+                            </Button>
+                            <a href="auth/redirect" variant="outline" className="w-full">
+                                Login with Google
+                            </a>
+                        </div>
+                        <div className="mt-4 text-center text-sm">
+                            Don&apos;t have an account?{" "}
+                            <Link
+                                href={route("register")}
+                                className="underline underline-offset-4"
+                            >
+                                Sign up
+                            </Link>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
