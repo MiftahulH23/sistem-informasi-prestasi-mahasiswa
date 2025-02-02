393f6fac500585d9dfa4ab783456993172ee4159
MiftahulH23 <miftahul21si@mahasiswa.pcr.ac.id>
2025-02-02 16:49:06 +0700

    Add UI components and update routes for About page

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+    return (
+        <AuthenticatedLayout>
+            <Head title="Dashboard" />
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            About
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+};
 
-export default About
+export default About;
