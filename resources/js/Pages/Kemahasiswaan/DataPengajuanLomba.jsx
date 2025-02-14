a5721d1ce31cd2fbfe75e6ca0568824c59a96856
MiftahulH23 <miftahul21si@mahasiswa.pcr.ac.id>
2025-02-14 22:15:01 +0700

    Refactor route definitions for Pengajuan Lomba, Kategori Lomba, and Judul Lomba; group routes by middleware for better organization

--- resources/js/Pages/Kemahasiswaan/DataPengajuanLomba.jsx
+++ resources/js/Pages/Kemahasiswaan/DataPengajuanLomba.jsx
@@ -1,10 +1,20 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, router } from "@inertiajs/react";
+import { useState } from "react";
 import Swal from "sweetalert2";
+
 const DataPengajuanLomba = ({ pengajuanLomba }) => {
+    const [reviewed, setReviewed] = useState(
+        pengajuanLomba.reduce((acc, lomba) => {
+            acc[lomba.id] = lomba.status !== "Diajukan";
+            return acc;
+        }, {})
+    );
+
     const updateStatus = (id, status) => {
         router.put(`/pengajuan-lomba/${id}/update-status`, { status });
         Swal.fire("Berhasil!", "Status pengajuan berhasil diubah.", "success");
+        setReviewed((prev) => ({ ...prev, [id]: true }));
     };
 
     return (
@@ -14,7 +24,7 @@ const DataPengajuanLomba = ({ pengajuanLomba }) => {
                 <table className="table">
                     {/* Head */}
                     <thead>
-                        <tr className=" text-center">
+                        <tr className="text-center">
                             <th className="px-4 py-2">No</th>
                             <th className="px-4 py-2">Kategori Lomba</th>
                             <th className="px-4 py-2">Judul Lomba</th>
@@ -30,95 +40,75 @@ const DataPengajuanLomba = ({ pengajuanLomba }) => {
                             pengajuanLomba.map((lomba, index) => (
                                 <tr key={lomba.id} className="text-center">
                                     <td className="px-4 py-2">{index + 1}</td>
+                                    <td className="px-4 py-2">{lomba.kategori?.kategori_lomba || "-"}</td>
+                                    <td className="px-4 py-2">{lomba.judul_lomba}</td>
+                                    <td className="px-4 py-2">{lomba.jenis_lomba}</td>
                                     <td className="px-4 py-2">
-                                        {lomba.kategori?.kategori_lomba || "-"}
-                                    </td>
-                                    <td className="px-4 py-2">
-                                        {lomba.judul_lomba}
-                                    </td>
-                                    <td className="px-4 py-2">
-                                        {lomba.jenis_lomba}
-                                    </td>
-                                    <td className="px-4 py-2">
-                                        {Array.isArray(
-                                            lomba.anggota_kelompok
-                                        ) && lomba.anggota_kelompok.length > 0
+                                        {Array.isArray(lomba.anggota_kelompok) && lomba.anggota_kelompok.length > 0
                                             ? lomba.anggota_kelompok.join(", ")
                                             : "-"}
                                     </td>
-
-                                    <td className="px-4 py-2">
-                                        {lomba.dosen_pembimbing}
-                                    </td>
+                                    <td className="px-4 py-2">{lomba.dosen_pembimbing}</td>
                                     <td
                                         className={`px-4 py-2 text-center 
-    ${lomba.status === "Diajukan" ? "text-blue-500" : ""}
-    ${lomba.status === "Diterima" ? "text-green-500" : ""}
-    ${lomba.status === "Ditolak" ? "text-red-500" : ""}`}
+                                            ${lomba.status === "Diajukan" ? "text-blue-500" : ""}
+                                            ${lomba.status === "Diterima" ? "text-green-500" : ""}
+                                            ${lomba.status === "Ditolak" ? "text-red-500" : ""}`}
                                     >
                                         {lomba.status}
                                     </td>
-
-                                    <td className="px-4 py-2  gap-2 grid place-items-center h-16">
-                                        <div className="flex gap-2 items-center justify-center">
-                                            <button
-                                                onClick={() =>
-                                                    updateStatus(
-                                                        lomba.id,
-                                                        "Diterima"
-                                                    )
-                                                }
-                                                className="bg-blue-500 text-white px-1 rounded size-5"
-                                            >
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="24"
-                                                    height="24"
-                                                    viewBox="0 0 24 24"
-                                                    fill="none"
-                                                    stroke="currentColor"
-                                                    strokeWidth="2"
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    className="lucide lucide-check w-full h-full object-cover"
+                                    <td className="px-4 py-2 gap-2 grid place-items-center h-16">
+                                        {reviewed[lomba.id] ? (
+                                            <span className="text-gray-500">Sudah Direview</span>
+                                        ) : (
+                                            <div className="flex gap-2 items-center justify-center">
+                                                <button
+                                                    onClick={() => updateStatus(lomba.id, "Diterima")}
+                                                    className="bg-blue-500 text-white px-1 rounded size-5"
                                                 >
-                                                    <path d="M20 6 9 17l-5-5" />
-                                                </svg>
-                                            </button>
-                                            <button
-                                                onClick={() =>
-                                                    updateStatus(
-                                                        lomba.id,
-                                                        "Ditolak"
-                                                    )
-                                                }
-                                                className="bg-red-500 text-white px-1 rounded size-5"
-                                            >
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="24"
-                                                    height="24"
-                                                    viewBox="0 0 24 24"
-                                                    fill="none"
-                                                    stroke="currentColor"
-                                                    strokeWidth="2"
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    className="lucide lucide-x w-full h-full object-cover"
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        width="24"
+                                                        height="24"
+                                                        viewBox="0 0 24 24"
+                                                        fill="none"
+                                                        stroke="currentColor"
+                                                        strokeWidth="2"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        className="lucide lucide-check w-full h-full object-cover"
+                                                    >
+                                                        <path d="M20 6 9 17l-5-5" />
+                                                    </svg>
+                                                </button>
+                                                <button
+                                                    onClick={() => updateStatus(lomba.id, "Ditolak")}
+                                                    className="bg-red-500 text-white px-1 rounded size-5"
                                                 >
-                                                    <path d="M18 6 6 18" />
-                                                    <path d="m6 6 12 12" />
-                                                </svg>
-                                            </button>
-                                        </div>
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        width="24"
+                                                        height="24"
+                                                        viewBox="0 0 24 24"
+                                                        fill="none"
+                                                        stroke="currentColor"
+                                                        strokeWidth="2"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        className="lucide lucide-x w-full h-full object-cover"
+                                                    >
+                                                        <path d="M18 6 6 18" />
+                                                        <path d="m6 6 12 12" />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        )}
                                     </td>
                                 </tr>
                             ))
                         ) : (
                             <tr>
-                                <td colSpan="8" className="text-center py-4">
-                                    Tidak ada data pengajuan lomba.
-                                </td>
+                                <td colSpan="8" className="text-center py-4">Tidak ada data pengajuan lomba.</td>
                             </tr>
                         )}
                     </tbody>
--- routes/web.php
+++ routes/web.php
@@ -3,51 +3,58 @@
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\SocialiteController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\KategoriLombaController;
+use App\Http\Controllers\JudulLombaController;
 use App\Http\Controllers\PengajuanLombaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\KategoriLombaController;
-use App\Http\Controllers\JudulLombaController;
 
-
+// Authentication Routes
 Route::get('/', [AuthenticatedSessionController::class, 'create'])->middleware('guest')->name('register');
+Route::get('/auth/redirect', [SocialiteController::class, 'redirect']);
+Route::get('/auth/google/callback', [SocialiteController::class, 'callback']);
+Route::post('/logout', [SocialiteController::class, 'logout'])->name('logout');
+require __DIR__ . '/auth.php';
 
-// Dashboard Controller
+// Dashboard
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
-// Pengajuan Lomba Controller
-Route::get('/pengajuan-lomba', [PengajuanLombaController::class, 'create'])->middleware(['auth', 'verified'])->name('pengajuan-lomba');
-Route::post('/pengajuan-lomba/store', [PengajuanLombaController::class, 'store'])->middleware(['auth', 'verified'])->name('pengajuan-lomba.store');
-Route::get('/data-pengajuan-lomba', [PengajuanLombaController::class, 'index'])->middleware(['auth', 'verified'])->name('data-pengajuan-lomba');
-Route::get('/update-pengajuan-lomba', [PengajuanLombaController::class, 'indexKemahasiswaan'])->middleware(['auth', 'verified'])->name('update-pengajuan-lomba');
-Route::put('/pengajuan-lomba/{id}/update-status', [PengajuanLombaController::class, 'updateStatus'])->name('pengajuan-lomba.update-status');
 
-// Kategori Lomba Controller
-Route::get('/kategori-lomba',[KategoriLombaController::class, 'index'])->middleware(['auth', 'verified'])->name('kategori-lomba');
-Route::post('/kategori-lomba/store', [KategoriLombaController::class, 'store'])->name('kategori-lomba.store');
-Route::put('/kategori-lomba/{kategoriLomba}', [KategoriLombaController::class, 'update'])->name('kategori-lomba.update');
-Route::delete('/kategori-lomba/{kategoriLomba}', [KategoriLombaController::class, 'destroy'])->name('kategori-lomba.destroy');
+// Pengajuan Lomba
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/pengajuan-lomba', [PengajuanLombaController::class, 'create'])->name('pengajuan-lomba');
+    Route::post('/pengajuan-lomba/store', [PengajuanLombaController::class, 'store'])->name('pengajuan-lomba.store');
+    Route::get('/data-pengajuan-lomba', [PengajuanLombaController::class, 'index'])->name('data-pengajuan-lomba');
+    Route::get('/update-pengajuan-lomba', [PengajuanLombaController::class, 'indexKemahasiswaan'])->name('update-pengajuan-lomba');
+    Route::put('/pengajuan-lomba/{id}/update-status', [PengajuanLombaController::class, 'updateStatus'])->name('pengajuan-lomba.update-status');
+});
+
+// Kategori Lomba
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/kategori-lomba',[KategoriLombaController::class, 'index'])->name('kategori-lomba');
+    Route::post('/kategori-lomba/store', [KategoriLombaController::class, 'store'])->name('kategori-lomba.store');
+    Route::put('/kategori-lomba/{kategoriLomba}', [KategoriLombaController::class, 'update'])->name('kategori-lomba.update');
+    Route::delete('/kategori-lomba/{kategoriLomba}', [KategoriLombaController::class, 'destroy'])->name('kategori-lomba.destroy');
+});
 
 // Judul Lomba
-Route::get('/judul-lomba', [JudulLombaController::class, 'index'])->middleware(['auth', 'verified'])->name('judul-lomba');
-Route::post('/judul-lomba/store', [JudulLombaController::class, 'store'])->name('judul-lomba.store');
-Route::put('/judul-lomba/{judulLomba}', [JudulLombaController::class, 'update'])->name('judul-lomba.update');
-Route::delete('/judul-lomba/{judulLomba}', [JudulLombaController::class, 'destroy'])->name('judul-lomba.destroy');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/judul-lomba', [JudulLombaController::class, 'index'])->name('judul-lomba');
+    Route::post('/judul-lomba/store', [JudulLombaController::class, 'store'])->name('judul-lomba.store');
+    Route::put('/judul-lomba/{judulLomba}', [JudulLombaController::class, 'update'])->name('judul-lomba.update');
+    Route::delete('/judul-lomba/{judulLomba}', [JudulLombaController::class, 'destroy'])->name('judul-lomba.destroy');
+});
 
+// Prestasi
 Route::get('/prestasi', function () {
     return Inertia::render('Prestasi');
 })->middleware(['auth', 'verified'])->name('prestasi');
 
+// Profile
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/auth/redirect', [SocialiteController::class, 'redirect']);
-Route::get('/auth/google/callback', [SocialiteController::class, 'callback']);
-Route::post('/logout', [SocialiteController::class, 'logout'])->name('logout');
-
-require __DIR__ . '/auth.php';
