aafa4ffe318fc6ae8915e3306d73cff1be980811
MiftahulH23 <miftahul21si@mahasiswa.pcr.ac.id>
2025-04-20 22:05:00 +0700

    fix: update import path casing for button variants in pagination component

--- resources/js/Components/ui/pagination.jsx
+++ resources/js/Components/ui/pagination.jsx
@@ -2,7 +2,7 @@ import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/Components/ui/button";
 
 const Pagination = ({
   className,
