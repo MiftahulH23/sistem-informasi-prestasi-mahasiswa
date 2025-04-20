3fc8a8cdceadc20801df604f3b9ae68b15fba3e7
MiftahulH23 <miftahul21si@mahasiswa.pcr.ac.id>
2025-04-20 22:01:35 +0700

    fix: update import path casing for Avatar components in Profile

--- resources/js/Components/Profile.jsx
+++ resources/js/Components/Profile.jsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/Components/ui/avatar";
 import { Button } from "@/Components/ui/button";
 import {
     DropdownMenu,
