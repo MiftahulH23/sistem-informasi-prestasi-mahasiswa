273346592c8cf47a298a4ed681edb2463600962d
MiftahulH23 <miftahul21si@mahasiswa.pcr.ac.id>
2025-04-22 00:39:27 +0700

    fix: update APP_URL in .env for local development and refactor Login component code style

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,20 +1,20 @@
-import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, Link, useForm } from '@inertiajs/react';
-import GoogleLogo from '@/Assets/images/googleLogo.png'
-import LogoKemahasiswaan from '@/Assets/images/LogoKemahasiswaanTransparant.png'
+import GuestLayout from "@/Layouts/GuestLayout";
+import { Head, Link, useForm } from "@inertiajs/react";
+import GoogleLogo from "@/Assets/images/googleLogo.png";
+import LogoKemahasiswaan from "@/Assets/images/LogoKemahasiswaanTransparant.png";
 
-export default function Login({ status, canResetPassword, appUrl  }) {
+export default function Login({ status, canResetPassword, appUrl }) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
+        email: "",
+        password: "",
         remember: false,
     });
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('login'), {
-            onFinish: () => reset('password'),
+        post(route("login"), {
+            onFinish: () => reset("password"),
         });
     };
 
@@ -27,22 +27,33 @@ export default function Login({ status, canResetPassword, appUrl  }) {
                     {status}
                 </div>
             )}
-            <div className='flex gap-3 items-center mb-4 justify-center'>
-                <div className='w-12 h-12 overflow-hidden'>
-                    <img src={LogoKemahasiswaan} alt="Logo Kemahasiswaan" className='w-full h-full object-cover' />
+            <div className="flex gap-3 items-center mb-4 justify-center">
+                <div className="w-12 h-12 overflow-hidden">
+                    <img
+                        src={LogoKemahasiswaan}
+                        alt="Logo Kemahasiswaan"
+                        className="w-full h-full object-cover"
+                    />
                 </div>
                 <div>
-                    <p className='text-primary font-bold text-xl'>Sistem Informasi Prestasi Mahasiswa</p>
-                    <p className='text-primary'>Kemahasiswaan PCR</p>
+                    <p className="text-primary font-bold text-xl">
+                        Sistem Informasi Prestasi Mahasiswa
+                    </p>
+                    <p className="text-primary">Kemahasiswaan PCR</p>
                 </div>
             </div>
-            <a href={`${appUrl}/auth/redirect`} className='w-full bg-background text-sky-500 px-4 py-2 rounded-lg shadow-md font-semibold flex gap-3 items-center justify-center hover:bg-[#009ef7] hover:text-white cursor-pointer'>
+            <a
+                href="auth/redirect"
+                className="w-full bg-background text-sky-500 px-4 py-2 rounded-lg shadow-md font-semibold flex gap-3 items-center justify-center hover:bg-[#009ef7] hover:text-white cursor-pointer"
+            >
                 <div className="w-6 h-6 overflow-hidden">
-                    <img src={GoogleLogo} alt="logo google" className='w-full h-full object-cover' />
+                    <img
+                        src={GoogleLogo}
+                        alt="logo google"
+                        className="w-full h-full object-cover"
+                    />
                 </div>
-                <div>
-                    Sign in with Google
-                </div>
+                <div>Sign in with Google</div>
             </a>
         </GuestLayout>
     );
