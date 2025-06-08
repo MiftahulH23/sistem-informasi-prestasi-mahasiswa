8531a14ed389b60c17f97fdddb205bdb7eeac614
Miftahul Huda <miftahul21si@mahasiswa.pcr.ac.id>
2025-06-08 17:02:29 +0700

    fix: Remove redundant description meta tag in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="Sistem Informasi Prestasi Mahasiswa Politeknik Caltex Riau" />
     <meta property="og:title" content="SIPRESMA PCR" />
     <meta name="description" content="SIPRESMA adalah sistem informasi prestasi mahasiswa Politeknik Caltex Riau." />
     <meta name="keywords"
